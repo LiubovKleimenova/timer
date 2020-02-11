ed48a26c96908b79cc4fc245a86c03d1c29bb863
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-11 12:37:24 -0500

    refactored spinner fn

--- timer1.js
+++ timer1.js
@@ -1,9 +1,10 @@
 let arr = process.argv.slice(2);
 let i = 0;
 for (const time of arr) {
+  if (time > 0) {
   i += Number(time);
-  console.log(i)
+  //console.log(i);
   setTimeout(() => {
     process.stdout.write("\x07");
   }, i);
-}
+}}
