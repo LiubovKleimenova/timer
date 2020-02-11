78bac417cbbceaa5bb6a456f534f62d5f60241be
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-02-11 13:09:04 -0500

    modified timer

--- timer1.js
+++ timer1.js
@@ -2,7 +2,7 @@ let arr = process.argv.slice(2);
 let i = 0;
 for (const time of arr) {
   if (time > 0) {
-  i += Number(time);
+  i += Number(time) * 1000;
   //console.log(i);
   setTimeout(() => {
     process.stdout.write("\x07");
